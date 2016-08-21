b279ce0606f1bfc17c796b7359012167922e4bcf
p4tin <paul@cp-soft.com>
2016-08-21 07:44:36 -0400

    Issue #68 - Fix for no queue on publish - Also updated code to go1.7

--- gosns/gosns_test.go
+++ gosns/gosns_test.go
@@ -110,6 +110,80 @@ func TestPublishhandler_POST_SendMessage(t *testing.T) {
 	}
 }
 
+func TestSubscribehandler_POST_Success(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	form := url.Values{}
+	form.Add("TopicArn", "arn:aws:sns:local:000000000000:UnitTestTopic1")
+	form.Add("Protocol", "sqs")
+	form.Add("Endpoint", "http://localhost:4100/queue/noqueue1")
+	req.PostForm = form
+
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Subscribe)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := "</SubscriptionArn>"
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestPublish_No_Queue_Error_handler_POST_Success(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	form := url.Values{}
+	form.Add("TopicArn", "arn:aws:sns:local:000000000000:UnitTestTopic1")
+	form.Add("Message", "TestMessage1")
+	req.PostForm = form
+
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Publish)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := "<MessageId>"
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+
 func TestDeleteTopichandler_POST_Success(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
@@ -151,3 +225,4 @@ func TestDeleteTopichandler_POST_Success(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
