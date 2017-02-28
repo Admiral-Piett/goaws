9250628d09bed0da51474695bf70e4c45237468f
p4tin <paul@cp-soft.com>
2017-02-28 10:00:57 -0500

    #87 - Check for non-existing queue before trying to put the message on it.

--- app/gosqs/gosqs_test.go
+++ app/gosqs/gosqs_test.go
@@ -73,3 +73,42 @@ func TestCreateQueuehandler_POST_CreateQueue(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+
+func TestSendQueue_POST_NonExistant(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	form := url.Values{}
+	form.Add("Action", "SendMessage")
+	form.Add("QueueUrl", "http://localhost:4100/queue/NON-EXISTANT")
+	form.Add("MessageBody", "Test123")
+	form.Add("Version", "2012-11-05")
+	req.PostForm = form
+
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(SendMessage)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := "NonExistentQueue"
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
